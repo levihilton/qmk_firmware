7bb5a9818aa58c3cd6cb70ae40cfbb061ae96b36
levihilton <levi.hilton.xlr8@gmail.com>
2020-09-20 11:25:17 -0600

    fixed last matrix issue

--- keyboards/handwired/flat_albatross/config.h
+++ keyboards/handwired/flat_albatross/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 7
 #define MATRIX_COLS 16
 #define MATRIX_ROW_PINS { C7, D5, D4, D6, D7, B4, B5 }
-#define MATRIX_COL_PINS { C6, D3, D2, D1, D0, B7, B3, E6,   F0, F1, F4, F5, F6, F7, B6, B5}
+#define MATRIX_COL_PINS { C6, D3, D2, D1, D0, B7, B3, E6,   F0, F1, F4, F5, B2, F6, F7, B6}
 
 #define MOUSEKEY_INTERVAL     20
 
